477fe279ed544f0e5d56065b977ffc7ec8a9cf6a
Eva Lam <eva.lam@code.berlin>
2022-05-17 19:21:44 +0200

    add privacy setting

--- locatebeacon/ViewController.swift
+++ locatebeacon/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
 
+    @IBOutlet weak var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    
+                }
+            }
+        }
+    }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 001, minor: 002, identifier: "mybeacon")
+        
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 1) {
+            switch distance {
+            
+            case .far:
+                self.view.backgroundColor = .blue
+                self.distanceReading.text = "far"
+            case .near:
+                self.view.backgroundColor = .orange
+                self.distanceReading.text = "near"
+            case .immediate:
+                self.view.backgroundColor = .red
+                self.distanceReading.text = "here"
+            default:
+                self.view.backgroundColor = .gray
+                self.distanceReading.text = "unknown"
+            }
+        }
+    }
 }
+
+
+
 
